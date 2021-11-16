f4aff6c8ab2db413d499db1b9dddf44f963c4adb
araceli <carmona.araceli.4690@gmail.com>
2021-11-15 22:25:49 -0800

    setting up firebase config

---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase/compat';
+
+const firebaseApp = firebase.initializeApp({
+    apiKey: "AIzaSyBFb1mahx7BreJmFQmszSRIrn0WBXf10UM",
+    authDomain: "insta-clone-7318b.firebaseapp.com",
+    projectId: "insta-clone-7318b",
+    storageBucket: "insta-clone-7318b.appspot.com",
+    messagingSenderId: "773069461517",
+    appId: "1:773069461517:web:7269117374cb1303139d44"
+});
+
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const storage = firebase.storage();
+
+export { db, auth, storage };
