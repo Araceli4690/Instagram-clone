37056e3bcee19c1accb3230e5abac03c3b199a9a
araceli <carmona.araceli.4690@gmail.com>
2021-11-15 21:52:37 -0800

    configuring useState for post mapping

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Logo from './images/logo.png'
 import Post from './components/Post';
 import './App.css';
 
 function App() {
+  const [posts, setPosts] = useState([
+    {
+      username: 'test',
+      caption: 'wow it works',
+      imageUrl: 'https://thumbs.dreamstime.com/b/cute-portrait-dog-pembroke-welsh-corgi-wear-cap-birthday-stretches-its-muzzle-to-delicious-cake-owner-hand-bright-pink-172610682.jpg'
+    },
+    {
+      username: 'test',
+      caption: 'wow it works',
+      imageUrl: 'https://thumbs.dreamstime.com/b/cute-portrait-dog-pembroke-welsh-corgi-wear-cap-birthday-stretches-its-muzzle-to-delicious-cake-owner-hand-bright-pink-172610682.jpg'
+    }
+  ]);
   return (
     <div className="App">
       {/* Header */}
@@ -11,9 +23,10 @@ function App() {
         <img className="app__headerImage" src={Logo} alt=""></img>
       </div>
       <h1> HELLO</h1>
-      <Post />
-      <Post />
-      <Post />
+      {posts.map(post => [
+        <Post username={post.username} caption={post.caption} imageUrl={post.imageUrl} />
+      ])}
+
       {/* Posts */}
       {/* Posts */}
     </div>
--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import './style.css'
 import Avatar from '@mui/material/Avatar'
 
-function Post() {
+function Post({ username, caption, imageUrl }) {
     return (
         <div className="post">
             <div className="post__header">
@@ -10,14 +10,17 @@ function Post() {
                 <Avatar className="post__avatar"
                     alt="Username"
                     src="images/avatar.jpeg" />
-                <h3>Username</h3>
+                <h3>{username}</h3>
             </div>
             {/*image */}
             <img className="post__image" src="https://thumbs.dreamstime.com/b/cute-portrait-dog-pembroke-welsh-corgi-wear-cap-birthday-stretches-its-muzzle-to-delicious-cake-owner-hand-bright-pink-172610682.jpg" alt="" />
             {/*usernaem -> caption */}
-            <h4 className="post__text"><strong>Username:</strong> caption</h4>
+            <h4 className="post__text"><strong>{username}</strong> {caption}</h4>
         </div>
     )
 }
 
 export default Post
+
+
+{/*Left off on 41:39 */ }
