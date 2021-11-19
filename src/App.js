05adaa7f5651722182379220bfb1d545b0b654ea
araceli <carmona.araceli.4690@gmail.com>
2021-11-18 22:50:02 -0800

    added modal styling

--- src/App.js
+++ src/App.js
@@ -4,6 +4,23 @@ import Post from './components/Post';
 import { db } from './firebase';
 import './App.css';
 import { ClassNames } from '@emotion/react';
+import Modal from '@mui/material/Modal';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
 
 function App() {
   const [posts, setPosts] = useState([]);
@@ -22,25 +39,34 @@ function App() {
       })))
     })
   }, []);
+  //signup function
+
   return (
     <div className="App">
+      <Button onClick={() => setOpen(true)}>Sign Up</Button>
       <Modal
         open={open}
-        onClose={handleClose}
+        //setting state of modal to false to close it
+        onClose={() => setOpen(false)}
       >
-        <div style={modalStyle} className={ClassNames.paper}>
-          <h2>Modal</h2>
-        </div>
+        <Box sx={style}>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            Text in a modal
+          </Typography>
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+          </Typography>
+        </Box>
       </Modal>
       {/* Header */}
       <div className="app__header">
         <img className="app__headerImage" src={Logo} alt=""></img>
       </div>
       <h1> HELLO</h1>
-      {posts.map(id, post => [
+      {posts.map(({ id, post }) => (
         //key lets react know to just update new post and not refresh old ones
         <Post key={id} username={post.username} caption={post.caption} imageUrl={post.imageUrl} />
-      ])}
+      ))}
 
       {/* Posts */}
       {/* Posts */}
